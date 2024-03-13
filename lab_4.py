ca72c0cb2874987952fe3a913de542b096140a9d
MilewskiMaciej <milewskimaciej@o2.pl>
2024-03-13 15:36:36 +0100

    lab_4

---
+++ lab_4.py
@@ -0,0 +1,14 @@
+import math
+
+#Zadanie 1
+def zad1():
+    print(round(((math.e**4)-(math.log2(34)))**(1/3), 2))
+    print(round((math.log(20)+math.cos(45)+math.e)**(1/3), 2))
+    print(round((math.log(20, 3)+math.sin(45)*(5/8))**(1/4), 2))
+    print(round(math.log(23, 3)+((math.sin(34)+5)**(1/3)), 2))
+    print(round((math.log2(32)+math.pi+math.sin(56))**(1/4), 2))
+
+def main():
+    zad1()
+
+main()
