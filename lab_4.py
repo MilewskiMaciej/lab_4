cdc891e2bd50ba3c0ffbf2f5fecda87f386b1182
MilewskiMaciej <milewskimaciej@o2.pl>
2024-03-13 15:53:36 +0100

    lab_4

--- lab_4.py
+++ lab_4.py
@@ -1,5 +1,6 @@
 import math
 
+"""
 #Zadanie 1
 def zad1():
     print(round(((math.e**4)-(math.log2(34)))**(1/3), 2))
@@ -8,7 +9,32 @@ def zad1():
     print(round(math.log(23, 3)+((math.sin(34)+5)**(1/3)), 2))
     print(round((math.log2(32)+math.pi+math.sin(56))**(1/4), 2))
 
-def main():
-    zad1()
+#Zadanie 2
+def zad2():
+    z = input("Podaj litere: ")
+    h = int(input("Podaj wysokosc wiezy (do 10): "))
+    if h > 10:
+        print("Podano zbyt duza liczbe.")
+    else:
+        while h != 0:
+            print(f"{z}{z}{z}{z}")
+            h=h-1
+        print("Przepraszam najmocniej, tylko takie wieze potrafie robic...")
 
+#Zadanie 3
+def zad3():
+    z = str(input("Podaj litere: "))
+    h = int(input("Podaj wysokosc piramidy (do 10): "))
+    if h > 10:
+        print("Podano zbyt duza liczbe.")
+    else:
+        for i in range(1, h+1):
+            print(i * z)
+            h=h-1
+"""
+
+def main():
+#    zad1()
+#    zad2()
+#    zad3()
 main()
